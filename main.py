0847def0cca12823fde05e95f4f55ab74cf3dfd7
matejagustin <gyllberta@gmail.com>
2015-12-14 16:14:27 +0100

    todo & film

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import jinja2
 import webapp2
 import random
 from models import Message
+from models import Todo
+from models import Movie
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
@@ -124,10 +126,12 @@ class CapitalHandler(BaseHandler,Capitals):
 
         states = [france, usa, egypt, italy, uk, greece, china, belgium, turkey, india]
 
+
         drzava = random.choice(states)
         state = drzava.state
         capital = drzava.city
         url = drzava.url
+
 
         is_visible = False
 
@@ -149,51 +153,6 @@ class CapitalHandler(BaseHandler,Capitals):
 
         return self.render_template("capital.html",params=params)
 
-class MessageHandler(BaseHandler):
-
-    def get(self):
-        self.render_template("hotel.html")
-
-    def post(self):
-        name = self.request.get("ime")
-        email = self.request.get("mail")
-        message = self.request.get("sporocilo")
-
-        whole_message = Message(ime=name,mail=email,sporocilo=message)
-        whole_message.put()
-
-        self.write(name)
-        self.write(email)
-        self.write(message)
-
-
-class SeznamSporocilHandler(BaseHandler):
-    def get(self):
-        seznam = Message.query().fetch()
-        params = {"seznam":seznam}
-        return self.render_template("seznam_sporocil.html",params=params)
-
-class SporociloHandler(BaseHandler):
-    def get(self,sporocilo_id):
-        sporocilo = Message.get_by_id(int(sporocilo_id))
-        params = {"sporocilo":sporocilo}
-        self.render_template("sporocilo.html",params=params)
-
-class IzpisHandler(BaseHandler):
-    def get(self):
-        self.render_template("izpis.html")
-
-    def post(self):
-        name = self.request.get("ime")
-        email = self.request.get("mail")
-        message = self.request.get("sporocilo")
-
-
-        whole_message = Message(ime=name,mail=email,sporocilo=message)
-        whole_message.put()
-
-        params = {"name":name, "email":email,"message":message}
-        self.render_template("izpis.html",params=params)
 
 class ConverterHandler(BaseHandler):
     def get(self):
@@ -277,7 +236,321 @@ class ConverterHandler(BaseHandler):
             params = {"valueError":valueError,"unit":unit}
             self.render_template("converter.html",params=params)
 
+class ForensicHandler(BaseHandler):
+    def get(self):
+        self.render_template("forensic.html")
 
+    def post(self):
+        dnk_enter = self.request.get("dnk_enter")
+        dnk_enter = dnk_enter.upper()
+
+        crni_lasje = "CCAGCAATCGC"
+        rjavi_lasje = "GCCAGTGCCG"
+        oranzni_lasje = "TTAGCTATCGC"
+        kvadraten_obraz = "GCCACGG"
+        okrogel_obraz = "ACCACAA"
+        ovalen_obraz = "AGGCCTCA"
+        modre_oci = "TTGTGGTGGC"
+        zelene_oci = "GGGAGGTGGC"
+        rjave_oci = "AAGTAGTGAC"
+        moski = "TGCAGGAACTTC"
+        zenska = "TGAAGGACCTTC"
+        belec = "AAAACCTCA"
+        crnec = "CGACTACAG"
+        azijec = "CGCGGGCCG"
+
+        face = ""
+        hair = ""
+        eyes = ""
+        gender = ""
+        rase = ""
+        suspect = []
+        error = ""
+
+        if crni_lasje in dnk_enter:
+            hair = "black hair"
+            suspect.append(hair)
+        elif rjavi_lasje in dnk_enter:
+            hair = "brown hair"
+            suspect.append(hair)
+        elif oranzni_lasje in dnk_enter:
+            hair = "orange hair"
+            suspect.append(hair)
+
+        if kvadraten_obraz in dnk_enter:
+            face = "square face"
+            suspect.append(face)
+        elif okrogel_obraz in dnk_enter:
+            face = "round face"
+            suspect.append(face)
+        elif ovalen_obraz in dnk_enter:
+            face = "oval face"
+            suspect.append(face)
+
+        if modre_oci in dnk_enter:
+            eyes = "blue eyes"
+            suspect.append(eyes)
+        elif zelene_oci in dnk_enter:
+            eyes = "green eyes"
+            suspect.append(eyes)
+        elif rjave_oci in dnk_enter:
+            eyes = "brown eyes"
+            suspect.append(eyes)
+
+        if moski in dnk_enter:
+            gender = "male"
+            suspect.append(gender)
+        elif zenska in dnk_enter:
+            gender = "female"
+            suspect.append(gender)
+
+        if belec in dnk_enter:
+            rase = "white"
+            suspect.append(rase)
+        elif crnec in dnk_enter:
+            rase = "afroamerican"
+            suspect.append(rase)
+        elif azijec in dnk_enter:
+            rase = "asian"
+            suspect.append(rase)
+
+
+        if len(suspect) <= 0:
+            error = "enter a valid dnk code"
+
+
+        params = {"hair":hair,"face":face,"eyes":eyes,"gender":gender,"rase":rase,"suspect":suspect,"error":error}
+        self.render_template("forensic.html",params=params)
+
+class MessageHandler(BaseHandler):
+
+    def get(self):
+        self.render_template("hotel.html")
+
+    def post(self):
+        name = self.request.get("ime")
+        email = self.request.get("mail")
+        message = self.request.get("sporocilo")
+
+        whole_message = Message(ime=name,mail=email,sporocilo=message)
+        whole_message.put()
+
+        self.write(name)
+        self.write(email)
+        self.write(message)
+
+
+class SeznamSporocilHandler(BaseHandler):
+    def get(self):
+        seznam = Message.query(Message.izbrisan == False).fetch()         # da prikazes samo tiste, ki niso izbrisani (v queriju)
+        params = {"seznam":seznam}
+        return self.render_template("seznam_sporocil.html",params=params)
+
+class SporociloHandler(BaseHandler):
+    def get(self,sporocilo_id):
+        sporocilo = Message.get_by_id(int(sporocilo_id))
+        params = {"sporocilo":sporocilo}
+        self.render_template("sporocilo.html",params=params)
+
+class IzpisHandler(BaseHandler):
+    def get(self):
+        self.render_template("izpis.html")
+
+    def post(self):
+        name = self.request.get("ime")
+        email = self.request.get("mail")
+        message = self.request.get("sporocilo")
+
+        whole_message = Message(ime=name,mail=email,sporocilo=message)
+        whole_message.put()
+
+        params = {"name":name, "email":email,"message":message}
+        self.render_template("izpis.html",params=params)
+
+class UrediSporociloHandler(BaseHandler):                       #ista koda, samo drugacen template in url
+    def get(self,sporocilo_id):
+        sporocilo = Message.get_by_id(int(sporocilo_id))
+        params = {"sporocilo":sporocilo}
+        return self.render_template("uredi_sporocilo.html",params=params)
+
+    def post(self,sporocilo_id):
+        vnos = self.request.get("vnos")
+        sporocilo = Message.get_by_id(int(sporocilo_id))
+        sporocilo.sporocilo = vnos
+
+        sporocilo.put()
+
+        self.redirect_to("seznam_sporocil")         #to je ta url iz handlerja (ne ime template)
+
+
+
+class IzbrisiSporociloHandler(BaseHandler):
+    def get(self,sporocilo_id):
+        sporocilo = Message.get_by_id(int(sporocilo_id))
+        params = {"sporocilo":sporocilo}
+        return self.render_template("izbrisi_sporocilo.html",params=params)
+
+    def post(self,sporocilo_id):                                # ce hocemo trajno zbrisat sporocilo: sporocilo.key.delete() in potem redirect
+        sporocilo = Message.get_by_id(int(sporocilo_id))
+        sporocilo.izbrisan = True
+
+        sporocilo.put()
+
+        self.redirect_to("seznam_sporocil")
+
+########## TO-DO handlers.... ##########
+
+class TodoHandler(BaseHandler):
+    def get(self):
+        return self.render_template("todo.html")
+
+    def post(self):
+        title = self.request.get("title")
+        notes = self.request.get("notes")
+        author = self.request.get("author")
+
+        title = title.upper()
+
+        task = Todo(naslov=title,datum=date,vsebina=notes,avtor=author)
+        task.put()
+
+
+        self.write(title)
+        self.write(notes)
+        self.write(author)
+
+
+class TodoSeznamHandler(BaseHandler):
+    def get(self):
+        seznam = Todo.query(Todo.izbrisan == False).fetch()
+        params = {"seznam":seznam}
+        self.render_template("todo_seznam.html",params=params)
+
+
+class TodoTaskDetailsHandler(BaseHandler):
+    def get(self,task_id):
+        task = Todo.get_by_id(int(task_id))
+        params = {"task":task}
+        self.render_template("todo_details.html",params=params)
+
+
+class TodoIzpisHandler(BaseHandler):
+    def get(self):
+        self.render_template("todo_task.html")
+
+    def post(self):
+        title = self.request.get("title")
+        notes = self.request.get("notes")
+        author = self.request.get("author")
+
+        title = title.upper()
+
+        task = Todo(naslov=title,vsebina=notes,avtor=author)
+        task.put()
+
+        params = {"title":title,"notes":notes,"avtor":author}
+        self.render_template("todo_task.html",params=params)
+
+
+class TodoEditHandler(BaseHandler):                       #ista koda, samo drugacen template in url
+    def get(self,task_id):
+        task = Todo.get_by_id(int(task_id))
+        params = {"task":task}
+        return self.render_template("todo_uredi.html",params=params)
+
+    def post(self,task_id):
+        vnos = self.request.get("vnos")
+        task = Todo.get_by_id(int(task_id))
+        task.vsebina = vnos
+
+        task.put()
+
+        self.redirect_to("todo_seznam")         #to je ta url iz handlerja (ne ime template)
+
+
+
+class TodoDeleteHandler(BaseHandler):
+    def get(self,task_id):
+        task = Todo.get_by_id(int(task_id))
+        params = {"task":task}
+        return self.render_template("todo_izbrisi.html",params=params)
+
+    def post(self,task_id):                                # ce hocemo trajno zbrisat sporocilo: sporocilo.key.delete() in potem redirect
+        task = Todo.get_by_id(int(task_id))
+        task.izbrisan = True
+
+        task.put()
+
+        self.redirect_to("todo_seznam")
+
+
+class TodoCompletedHandler(BaseHandler):
+    def get(self,task_id):
+        task = Todo.get_by_id(int(task_id))
+        params = {"task":task}
+        return self.render_template("todo_completed.html",params=params)
+
+    def post(self,task_id):                                # ce hocemo trajno zbrisat sporocilo: sporocilo.key.delete() in potem redirect
+        task = Todo.get_by_id(int(task_id))
+        task.dokoncan = True
+
+        task.put()
+
+        self.redirect_to("todo_seznam")
+
+class TestHandler(BaseHandler):
+    def get(self):
+        self.render_template("test.html")
+
+    def post(self):
+        test1 = self.request.get("test1")
+        test2 = self.request.get("test2")
+        test3 = self.request.get("test3")
+
+        params = {"test1":test1,"test2":test2,"test3":test3}
+        return self.render_template("test.html",params=params)
+
+
+class MovieHandler(BaseHandler):
+
+    def get(self):
+        self.render_template("film.html")
+
+    def post(self):
+        title = self.request.get("title")
+        url = self.request.get("url")
+        rate = self.request.get("rate")
+
+        movie = Movie(title=title,url=url,rate=rate)
+        movie.put()
+
+        self.write(title)
+        self.write(url)
+        self.write(rate)
+
+
+
+class IzpisFilmaHandler(BaseHandler):
+    def get(self):
+        self.render_template("film_izpis.html")
+
+    def post(self):
+        title = self.request.get("title")
+        url = self.request.get("url")
+        rate = self.request.get("rate")
+
+        movie = Movie(title=title,url=url,rate=rate)
+        movie.put()
+
+
+        params = {"title":title,"url":url,"movie":movie,"rate":rate}
+        self.render_template("film_izpis.html",params=params)
+
+class FilmSeznamHandler(BaseHandler):
+    def get(self):
+        filmi = Movie.query().fetch()
+        params = {"filmi":filmi}
+        return self.render_template("film_seznam.html",params=params)
 
 app = webapp2.WSGIApplication([
     webapp2.Route('/', MainHandler),
@@ -288,8 +561,22 @@ app = webapp2.WSGIApplication([
     webapp2.Route('/calculator',CalculatorHandler),
     webapp2.Route('/capital',CapitalHandler),
     webapp2.Route('/hotel',MessageHandler),
-    webapp2.Route('/seznam_sporocil',SeznamSporocilHandler),
+    webapp2.Route('/seznam_sporocil',SeznamSporocilHandler, name="seznam_sporocil"),
     webapp2.Route('/sporocilo/<sporocilo_id:\d+>',SporociloHandler),
     webapp2.Route('/izpis',IzpisHandler),
+    webapp2.Route('/sporocilo/<sporocilo_id:\d+>/uredi',UrediSporociloHandler),
+    webapp2.Route('/sporocilo/<sporocilo_id:\d+>/izbrisi',IzbrisiSporociloHandler),
     webapp2.Route('/converter',ConverterHandler),
+    webapp2.Route('/forensic',ForensicHandler),
+    webapp2.Route('/todo',TodoHandler),
+    webapp2.Route('/todo_task',TodoIzpisHandler),
+    webapp2.Route('/todo_seznam',TodoSeznamHandler, name="todo_seznam"),
+    webapp2.Route('/todo_details/<task_id:\d+>',TodoTaskDetailsHandler),
+    webapp2.Route('/todo_details/<task_id:\d+>/edit',TodoEditHandler),
+    webapp2.Route('/todo_details/<task_id:\d+>/delete',TodoDeleteHandler),
+    webapp2.Route('/todo_details/<task_id:\d+>/completed',TodoCompletedHandler),
+    webapp2.Route('/test',TestHandler),
+    webapp2.Route('/film',MovieHandler),
+    webapp2.Route('/film_izpis',IzpisFilmaHandler),
+    webapp2.Route('/film_seznam',FilmSeznamHandler, name="film_seznam"),
 ], debug=True)
--- models.py
+++ models.py
@@ -5,3 +5,17 @@ class Message(ndb.Model):
     mail = ndb.StringProperty(required=False)
     sporocilo = ndb.StringProperty(required=True)
     nastanek = ndb.DateProperty(auto_now_add=True)
+    izbrisan = ndb.BooleanProperty(default=False)
+
+class Todo(ndb.Model):
+    naslov = ndb.StringProperty()
+    avtor = ndb.StringProperty()
+    vsebina = ndb.StringProperty()
+    nastanek = ndb.DateProperty(auto_now_add=True)
+    izbrisan = ndb.BooleanProperty(default=False)
+    dokoncan = ndb.BooleanProperty(default=False)
+
+class Movie(ndb.Model):
+    title = ndb.StringProperty()
+    url = ndb.StringProperty()
+    rate = ndb.StringProperty()
